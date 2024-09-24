fae9f18230eb3afb9e046834ac2ffe1fd470fd79
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2024-09-24 11:14:11 +0530

    Pass Array to components

--- fullstackopen/part1/part1/src/App.jsx
+++ fullstackopen/part1/part1/src/App.jsx
@@ -6,19 +6,31 @@ const Header = (props) => {
   )
 }
 const Content = (props) => {
-  return(
-  <>
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  </>
+  console.log(props)
+  return (
+    <div>
+      <Part part ={props.parts[0].name} exercises = {props.parts[0].exercises}/>
+      <Part part ={props.parts[1].name} exercises = {props.parts[1].exercises}/>
+      <Part part ={props.parts[2].name} exercises = {props.parts[2].exercises}/>
+    </div>
   )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return(
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+    )
 }
 
 const Total = (props) => {
   return(
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </>
   )
 } 
@@ -26,22 +38,31 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
   
   return (
     <div>
       <Header course = {course} />
-      <Content part = {part1} exercises = {exercises1} />
-      <Content part = {part2} exercises = {exercises2}/>
-      <Content part = {part3} exercises = {exercises3}/>
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+      <Content parts = {parts}/>
+      <Total parts = {parts} />
     </div>
   )
 }
 
 export default App
+
+
+
